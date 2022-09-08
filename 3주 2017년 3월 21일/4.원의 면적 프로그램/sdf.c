67d8df5b437734391af4d23fbded31cecd1b01bb
juyangjin <112486031+juyangjin@users.noreply.github.com>
2022-09-08 21:20:31 +0900

    Add files via upload

---
+++ 3주 2017년 3월 21일/4.원의 면적 프로그램/sdf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	float A;
+	float B;
+	float C;
+
+	printf("»ï°¢ÇüÀÇ ¹Øº¯:");
+	scanf("%f",&A);
+	printf("»ï°¢ÇüÀÇ ³ôÀÌ:");
+	scanf("%f",&B);
+	
+	C=A*B/2;
+
+	printf("»ï°¢ÇüÀÇ ³ĞÀÌ=%f",C);
+	return 0;
+}
